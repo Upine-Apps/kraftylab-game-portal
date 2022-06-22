6375ae30e1da3745c0b8a58d324d29484bab38cd
Tristan Nigos <tristanigos@gmail.com>
2022-06-22 18:25:22 -0500

    refactoring; convert to html button

--- src/components/buttons/ReusableButton.js
+++ src/components/buttons/ReusableButton.js
@@ -8,40 +8,34 @@ export default function ReusableButton(props) {
   // how will the button component obtain state from other fields? curiosity.
   // either lifting up and passing down state, or redux
 
-  const handleSubmit = async () => {
-    try {
-    } catch (err) {
-      console.error("error invoked")
-    }
-  }
-
   return (
-    <Wrapper>
-      <TextWrapper>
-        <Title onSubmit={handleSubmit}>{title || "Submit"}</Title>
-      </TextWrapper>
-    </Wrapper>
+    <>
+      <ButtonWrapper>
+        <button type="submit" value={title || "Submit"}>
+          {title || "Submit"}
+        </button>
+      </ButtonWrapper>
+    </>
   )
 }
 
-const Wrapper = styled.div`
-  max-width: 400px;
-  background: #0067ff;
-  border: 1px solid #0056d7;
-  border-radius: 4px;
-  flex-direction: row;
-  justify-content: center;
-  align-items: center;
-  padding: 14px 14px;
-  gap: 10px;
-`
+const ButtonWrapper = styled.div`
+  button {
+    max-width: 500px;
+    width: 400px;
+    background: #0067ff;
+    border: 1px solid #0056d7;
+    border-radius: 4px;
+    flex-direction: row;
+    justify-content: center;
+    align-items: center;
+    padding: 14px 14px;
+    gap: 10px;
 
-const TextWrapper = styled.div`
-  gap: 4px;
-`
+    color: white;
+    font-weight: 600;
+    font-size: 15px;
 
-const Title = styled.div`
-  color: white;
-  font-weight: 600;
-  font-size: 15px;
+    cursor: pointer;
+  }
 `
