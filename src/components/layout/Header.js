4a44082f164704282cf6b6633c52caeee45583f6
William <61369939+wJollie@users.noreply.github.com>
2022-06-24 17:20:05 -0400

    Removed Navbar, changed Logo

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -1,78 +1,28 @@
 import React, { useRef, useState } from "react"
 import { Link } from "gatsby"
 import styled from "styled-components"
-import { menuData } from "../../data/menuData"
-import MenuButton from "../buttons/MenuButton"
-import MenuTooltip from "../tooltips/MenuTooltip"
-import { useEffect } from "react"
 
 export default function Header() {
-  const [isOpen, setIsOpen] = useState(false)
 
-  const ref = useRef()
-
-  const tooltipRef = useRef()
-
-  function handleClick(event) {
-    setIsOpen(!isOpen)
-    event.preventDefault()
-  }
-
-  function handleClickOutside(event) {
-    if (
-      ref.current &&
-      !ref.current.contains(event.target) &&
-      !tooltipRef.current.contains(event.target)
-    ) {
-      setIsOpen(false)
-    }
-  }
-
-  useEffect(() => {
-    document.addEventListener("mousedown", handleClickOutside)
-
-    return () => {
-      document.removeEventListener("mousedown", handleClickOutside)
-    }
-  }, [])
-
-  return (
-    <Wrapper>
-      <LinkWrapper>
-        <Link to="/">
-          <img src="/images/logos/dark-logo-notext.svg" alt="Logo" />
-        </Link>
-      </LinkWrapper>
-      <MenuWrapper count={menuData.length} ref={ref}>
-        {menuData.map((item, index) =>
-          item.link === "/account" ? (
-            <MenuButton
-              onClick={event => handleClick(event)}
-              item={item}
-              key={index}
-            />
-          ) : (
-            <MenuButton item={item} key={index} />
-          )
-        )}
-        <HamburgerWrapper>
-          <MenuButton
-            item={{ title: "", icon: "/images/icons/hamburger.svg", link: "/" }}
-            onClick={event => handleClick(event)}
-          />
-        </HamburgerWrapper>
-      </MenuWrapper>
-      <div ref={tooltipRef}>
-        <MenuTooltip isOpen={isOpen} />
-      </div>
-    </Wrapper>
-  )
+    return (
+        <Wrapper>
+            <LinkWrapper>
+                <Link to="/">
+                    <img src="/images/logos/kraftylab-logo.svg" alt="Logo" />
+                </Link>
+            </LinkWrapper>
+        </Wrapper>
+    )
 }
 
 const LinkWrapper = styled.div`
   width: 75px;
   @media (max-width: 450px) {
     width: 44px;
+  }
+  img {
+    height: 100px;
+    width: 100px;
   }
 `
 
@@ -93,24 +43,4 @@ const Wrapper = styled.div`
     top: 20px;
     padding: 0 20px;
   }
-`
-
-const MenuWrapper = styled.div`
-  display: grid;
-  gap: 30px;
-  grid-template-columns: repeat(${props => props.count}, auto);
-
-  @media (max-width: 768px) {
-    > a {
-      display: none;
-    }
-    grid-template-columns: auto;
-  }
-`
-
-const HamburgerWrapper = styled.div`
-  display: none;
-  @media (max-width: 768px) {
-    display: block;
-  } ;
 `
