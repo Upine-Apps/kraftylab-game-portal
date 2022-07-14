2c2662f429cbab39cb7cf3b1b4f82145d95e4714
Casey Quinn <74877883+cquinn2020@users.noreply.github.com>
2022-07-14 10:46:53 -0500

    fixed merge conflicts

--- src/components/sections/auth/AuthSection.js
+++ src/components/sections/auth/AuthSection.js
@@ -1,43 +1,35 @@
-<<<<<<< HEAD
 import React from "react";
 import styled from "styled-components";
 import BackButton from "../../buttons/mobile/BackButton";
 import { useState, useEffect } from "react";
 import LoginPage from "../LoginPage";
-=======
-import React from "react"
-import styled from "styled-components"
-import BackButton from "../../buttons/mobile/BackButton"
-import { useState, useEffect } from "react"
-
->>>>>>> master
 export default function AuthSection() {
   const [screenSize, getDimension] = useState({
     dynamicWidth: window.innerWidth,
     dynamicHeight: window.innerHeight,
-  })
+  });
 
   const setDimension = () => {
     getDimension({
       dynamicWidth: window.innerWidth,
       dynamicHeight: window.innerHeight,
-    })
-  }
+    });
+  };
 
   useEffect(() => {
-    window.addEventListener("resize", setDimension)
+    window.addEventListener("resize", setDimension);
 
     return () => {
-      window.removeEventListener("resize", setDimension)
-    }
-  }, [screenSize])
+      window.removeEventListener("resize", setDimension);
+    };
+  }, [screenSize]);
 
   function renderDesktop() {
     return (
       <GraphicWrapper>
         <Graphic src="images/auth/auth-graphic.svg" />
       </GraphicWrapper>
-    )
+    );
   }
 
   return (
@@ -45,27 +37,18 @@ export default function AuthSection() {
       <ContentWrapper>
         {screenSize.dynamicWidth > 450 ? renderDesktop() : ""}
         <ScreenWrapper>
-<<<<<<< HEAD
           <LoginPage />
-=======
-          {/* ADD YOUR COMPONENT HERE INSTEAD OF SCREEN COMPONENT
-            SCREEN COMPONENT IS JUST THERE TO SHOW YOU THE RED BOX IT SHOULD TAKE UP. 
-            !!!!REMOVE SCREEN COMPONENT AND GRAPHIC!!!
-            MAKE SURE YOU LOOK AT HOW I ADDED WIDTH AND HEIGHT FOR THE COMPONENT
-            WE WANT THIS TO BE FLEXIBLE WITH WHATEVER BOX WE PUT IT IN
-            DON'T HARDCODE WIDTHS AND HEIGHTS W PIXELS!!!! USE PERCENTAGES */}
->>>>>>> master
         </ScreenWrapper>
       </ContentWrapper>
     </Wrapper>
-  )
+  );
 }
 
 const Wrapper = styled.div`
   position: absolute;
   height: 100%;
   width: 100%;
-`
+`;
 
 const ContentWrapper = styled.div`
   display: grid;
@@ -75,7 +58,7 @@ const ContentWrapper = styled.div`
   @media (max-width: 450px) {
     display: inline;
   }
-`
+`;
 
 const GraphicWrapper = styled.div`
   display: grid;
@@ -87,12 +70,12 @@ const GraphicWrapper = styled.div`
   @media (max-width: 450px) {
     display: hidden;
   }
-`
+`;
 
 const Graphic = styled.img`
   width: 75%;
   height: 100%;
-`
+`;
 
 const ScreenWrapper = styled.div`
   margin: auto 0;
@@ -104,7 +87,7 @@ const ScreenWrapper = styled.div`
     flex-direction: column;
     justify-content: center;
   }
-`
+`;
 
 const ScreenComponent = styled.div`
   display: grid;
@@ -113,4 +96,4 @@ const ScreenComponent = styled.div`
   height: 100%;
   justify-items: center;
   align-content: center;
-`
+`;
