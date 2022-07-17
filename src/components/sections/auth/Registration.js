82050090aeeee5af8427cbfed0b86d994fb0990e
Tristan Nigos <tristanigos@gmail.com>
2022-07-17 15:35:19 -0500

    persistent issue with cors

--- src/components/sections/auth/Registration.js
+++ src/components/sections/auth/Registration.js
@@ -1,29 +1,30 @@
-import React from "react";
-import styled from "styled-components";
-import { SmallText, H4 } from "../../styles/TextStyles";
-import { useState } from "react";
-import { themes } from "../../styles/ColorStyles";
-import ReusableTextField from "../../textfield/ReusableTextField";
-import CustomPasswordField from "../../textfield/CustomPasswordField";
-import ReusableButton from "../../buttons/ReusableButton";
-import TextButton from "../../buttons/TextButton";
+import React from "react"
+import styled from "styled-components"
+import { SmallText, H4 } from "../../styles/TextStyles"
+import { useState } from "react"
+import { themes } from "../../styles/ColorStyles"
+import ReusableTextField from "../../textfield/ReusableTextField"
+import CustomPasswordField from "../../textfield/CustomPasswordField"
+import ReusableButton from "../../buttons/ReusableButton"
+import TextButton from "../../buttons/TextButton"
 
-import axios from "axios";
+import axios from "axios"
 
 export default function Registration() {
-  const [firstName, setFirstName] = useState("");
-  const [lastName, setLastName] = useState("");
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
+  const [firstName, setFirstName] = useState("")
+  const [lastName, setLastName] = useState("")
+  const [email, setEmail] = useState("")
+  const [password, setPassword] = useState("")
+  const [confirmPassword, setConfirmPassword] = useState("")
 
   function onChange(e) {
-    console.log(e.target.name, e.target.value);
+    console.log(e.target.name, e.target.value)
   }
 
   async function onClick(e) {
-    console.log("clicked!");
+    console.log("clicked!")
     // FIXME: function will unmount component and mount a new one
-    e.preventDefault();
+    e.preventDefault()
 
     const body = {
       email: email,
@@ -31,24 +32,25 @@ export default function Registration() {
       last_name: lastName,
       validated: false,
       password: password,
-    };
+    }
 
     const headers = {
       "Content-Type": "application/json",
       "Access-Control-Allow-Origin": "*",
       "Access-Control-Request-Headers": "content-type",
-    };
+    }
 
-    console.log(body);
-    let response = await axios.post("http://localhost:3000/user", body, {
-      headers,
-    });
-    console.log(response);
+    // console.log(body)
+    // let response = await axios.post("http://localhost:3000/user", body, {
+    //   headers,
+    // })
+    // console.log(response)
 
-    setFirstName("");
-    setLastName("");
-    setEmail("");
-    setPassword("");
+    setFirstName("")
+    setLastName("")
+    setEmail("")
+    setPassword("")
+    setConfirmPassword("")
   }
 
   return (
@@ -60,21 +62,32 @@ export default function Registration() {
       <FormWrapper>
         <ReusableTextField
           title="First Name"
+          value={firstName}
           onChange={(e) => setFirstName(e.target.value)}
         />
         <ReusableTextField
           title="Last Name"
+          value={lastName}
           onChange={(e) => setLastName(e.target.value)}
         />
         <ReusableTextField
           title="Email"
+          value={email}
           onChange={(e) => setEmail(e.target.value)}
         />
         <CustomPasswordField
           name="Password"
           label="Password"
+          value={password}
           placeholder="Please enter your password"
           onChange={(e) => setPassword(e.target.value)}
+        />
+        <CustomPasswordField
+          name="Confirm Password"
+          label="Confirm Password"
+          value={confirmPassword}
+          placeholder="Please confirm your password"
+          onChange={(e) => setConfirmPassword(e.target.value)}
         />
         <ReusableButton title="Register" onClick={(e) => onClick(e)} />
         <TextButtonWrapper>
@@ -83,7 +96,7 @@ export default function Registration() {
         </TextButtonWrapper>
       </FormWrapper>
     </Wrapper>
-  );
+  )
 }
 
 const Wrapper = styled.div`
@@ -96,30 +109,30 @@ const Wrapper = styled.div`
     padding: 0 30px;
     max-width: none;
   }
-`;
+`
 
 const Title = styled(H4)`
   padding-bottom: 25px;
 
   background-clip: text;
   -webkit-background-clip: text;
-`;
+`
 
 const Subtitle = styled(SmallText)`
   padding: 15px 0;
   color: ${themes.light.text1};
-`;
+`
 
 const TextButtonWrapper = styled.div`
   display: grid;
   grid-template-columns: 125px auto;
   align-items: start;
-`;
+`
 
 const TextWrapper = styled.div`
   display: grid;
   text-align: left;
   gap: 0px;
-`;
+`
 
-const FormWrapper = styled.div``;
+const FormWrapper = styled.div``
