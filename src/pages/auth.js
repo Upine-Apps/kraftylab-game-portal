d50acf7586a9351262575223b568237857200fb4
Tristan Nigos <tristanigos@gmail.com>
2022-07-17 16:51:08 -0500

    authpage

--- src/pages/auth.js
+++ src/pages/auth.js
@@ -1,15 +1,15 @@
-import React from "react";
-import Layout from "../components/layout/layout";
-import SEO from "../components/layout/seo";
-import allReducer from "../reducers/index.js";
-import AuthSection from "../components/sections/AuthSection";
-import { configureStore } from "@reduxjs/toolkit";
+import React from "react"
+import Layout from "../components/layout/layout"
+import SEO from "../components/layout/seo"
+import allReducer from "../reducers/index.js"
+import AuthSection from "../components/sections/auth/AuthSection"
+import { configureStore } from "@reduxjs/toolkit"
 
 export default function AuthPage() {
-  const store = configureStore({ reducer: allReducer });
+  const store = configureStore({ reducer: allReducer })
   return (
     <Layout>
       <AuthSection />
     </Layout>
-  );
+  )
 }
