137b82c76ed4de934a7a479dfadf446204eb44da
Tate M Walker <github@notrack.dev>
2022-07-18 15:14:55 -0500

    button wrapper flex and path conditional (#25)

--- src/components/sections/auth/Registration.js
+++ src/components/sections/auth/Registration.js
@@ -1,24 +1,24 @@
-import React from "react"
-import styled from "styled-components"
+import React from "react";
+import styled from "styled-components";
 import {
   MediumText,
   Caption,
   SmallText,
   AuthTitle,
   H4,
-} from "../../styles/TextStyles"
-import { themes } from "../../styles/ColorStyles"
-import ReusableTextField from "../../textfield/ReusableTextField"
-import CustomPasswordField from "../../textfield/CustomPasswordField"
-import ReusableButton from "../../buttons/ReusableButton"
-import TextButton from "../../buttons/TextButton"
+} from "../../styles/TextStyles";
+import { themes } from "../../styles/ColorStyles";
+import ReusableTextField from "../../textfield/ReusableTextField";
+import CustomPasswordField from "../../textfield/CustomPasswordField";
+import ReusableButton from "../../buttons/ReusableButton";
+import TextButton from "../../buttons/TextButton";
 
 export default function Registration() {
   function onChange(e) {
-    console.log(e.target.value)
+    console.log(e.target.value);
   }
   function onClick() {
-    console.log("clicked!")
+    console.log("clicked!");
     // FIXME: function will unmount component and mount a new one
   }
 
@@ -44,7 +44,7 @@ export default function Registration() {
         </TextButtonWrapper>
       </FormWrapper>
     </Wrapper>
-  )
+  );
 }
 
 const Wrapper = styled.div`
@@ -57,30 +57,30 @@ const Wrapper = styled.div`
     padding: 0 30px;
     max-width: none;
   }
-`
+`;
 
 const Title = styled(H4)`
   padding-bottom: 25px;
 
   background-clip: text;
   -webkit-background-clip: text;
-`
+`;
 
 const Subtitle = styled(SmallText)`
   padding: 15px 0;
   color: ${themes.light.text1};
-`
+`;
 
 const TextButtonWrapper = styled.div`
-  display: grid;
-  grid-template-columns: 125px auto;
+  display: flex;
   align-items: start;
-`
+  gap: 10px;
+`;
 
 const TextWrapper = styled.div`
   display: grid;
   text-align: left;
   gap: 0px;
-`
+`;
 
-const FormWrapper = styled.div``
+const FormWrapper = styled.div``;
