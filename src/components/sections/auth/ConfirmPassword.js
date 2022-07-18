2c0494d3829da0b1836b4d8af7afc3c9c4af3125
Casey Quinn <74877883+cquinn2020@users.noreply.github.com>
2022-07-18 21:32:09 +0000

    Ua 016 (#24)
    * Created the file for writing Confirm Password Screen.
    
    * Created Confirm Password screen based on Login Page and Tristans work. Images are still not showing up locally while testing.
    
    * added the path= arg for props in the TextButton and Reusable Button
    
    * Update ConfirmPassword.js
    
    cleaned up
    
    Co-authored-by: Tate M Walker <github@notrack.dev>

---
+++ src/components/sections/auth/ConfirmPassword.js
@@ -0,0 +1,82 @@
+import styled from "styled-components";
+import {
+  MediumText,
+  Caption,
+  SmallText,
+  AuthTitle,
+  H4,
+  H3,
+} from "../../styles/TextStyles";
+import { themes } from "../../styles/ColorStyles";
+import ReusableTextField from "../../textfield/ReusableTextField";
+import CustomPasswordField from "../../textfield/CustomPasswordField";
+import ReusableButton from "../../buttons/ReusableButton";
+import React, { useState } from "react";
+import TextButton from "../../buttons/TextButton";
+
+export default function ConfirmPassword() {
+  function onChange(e) {
+    console.log(e.target.value);
+  }
+  return (
+    <Wrapper>
+      <TextWrapper>
+        <Subtitle>Whew! 👋</Subtitle>
+        <Title>Create new password</Title>
+      </TextWrapper>
+      <FormWrapper>
+        <CustomPasswordField
+          name="New Password"
+          label="New Password"
+          placeholder="Please enter your new password"
+        />
+        <CustomPasswordField
+          name="Confirm Password"
+          label="Confirm Password"
+          placeholder="Please confirm your new password"
+        />
+        <ReusableButton title="Reset Password" path="" />
+        <TextButtonWrapper>
+          <Subtitle>Remember your password?</Subtitle>
+          <TextButton title="Login" path=""></TextButton>
+        </TextButtonWrapper>
+      </FormWrapper>
+    </Wrapper>
+  );
+}
+
+const Wrapper = styled.div`
+  justify-items: center;
+  margin: 0 auto; // look into what this is doing
+  max-width: 400px;
+  @media (max-width: 450px) {
+    vertical-align: middle;
+    margin: 0;
+    padding: 0 30px;
+    max-width: none;
+  }
+`;
+const TextButtonWrapper = styled.div`
+  display: flex;
+  align-items: start;
+  gap: 10px;
+`;
+
+const Title = styled(H4)`
+  padding-bottom: 25px;
+  background-clip: text;
+  -webkit-background-clip: text;
+`;
+
+const Subtitle = styled(SmallText)`
+  padding: 15px 0;
+  color: ${themes.light.text1};
+`;
+
+const TextWrapper = styled.div`
+  display: grid;
+  text-align: left;
+  gap: 0px;
+`;
+
+const FormWrapper = styled.div``;
