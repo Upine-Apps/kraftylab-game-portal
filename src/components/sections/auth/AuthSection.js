ebc24035b0b41a7a50cb587550fa53f9c690fcfa
William <61369939+wJollie@users.noreply.github.com>
2022-07-18 16:32:42 -0400

    Ua 015 (#26)
    * Create Verification.js
    
    Created Verification Screen
    
    * Display: flex

--- src/components/sections/auth/AuthSection.js
+++ src/components/sections/auth/AuthSection.js
@@ -37,6 +37,7 @@ export default function AuthSection() {
       <ContentWrapper>
         {screenSize.dynamicWidth > 450 ? renderDesktop() : ""}
         <ScreenWrapper>
+          <Verification></Verification>
           {/* ADD YOUR COMPONENT HERE INSTEAD OF SCREEN COMPONENT
             SCREEN COMPONENT IS JUST THERE TO SHOW YOU THE RED BOX IT SHOULD TAKE UP. 
             !!!!REMOVE SCREEN COMPONENT AND GRAPHIC!!!
