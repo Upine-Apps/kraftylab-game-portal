95fe66efb5ee335dfa9e4b00adbd89a6a024d2f1
Tristan Nigos <tristanigos@gmail.com>
2022-08-18 20:59:57 -0500

    icebreaker dir

---
+++ src/pages/icebreaker.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Layout from "../components/layout/layout";
+import IcebreakerSection from "../components/sections/icebreaker/IcebreakerSection";
+
+function IcebreakerPage() {
+  return (
+    <Layout>
+      <IcebreakerSection />
+    </Layout>
+  );
+}
+
+export default IcebreakerPage;
