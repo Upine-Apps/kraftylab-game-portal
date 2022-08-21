cbcde6a1aa91d74e42dfbfbbdd5746db12c5a580
William <61369939+wJollie@users.noreply.github.com>
2022-08-21 17:27:07 -0400

    Update gameAllData.js

--- src/data/gameAllData.js
+++ src/data/gameAllData.js
@@ -7,7 +7,7 @@ export const gameAllData = [
     description: "Get to know each other!",
     color:
       "radial-gradient(218.51% 281.09% at 100% 100%, rgba(253, 63, 51, 0.7) 0%, rgba(76, 0, 200, 0.7) 45.83%, rgba(76, 0, 200, 0.7) 100%)",
-    icon: "Test",
+    icon: "<img src={skill.icon} />",
   },
   {
     title: "Bingo",
