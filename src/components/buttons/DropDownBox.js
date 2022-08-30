b40ddcedfd2c2ca4cab65abd0b0f28fd7cfed130
William <61369939+wJollie@users.noreply.github.com>
2022-08-29 22:22:40 -0400

    Update DropDownBox.js

--- src/components/buttons/DropDownBox.js
+++ src/components/buttons/DropDownBox.js
@@ -1,6 +1,7 @@
 import Dropdown from "react-dropdown";
 import "react-dropdown/style.css";
 import React from "react";
+import styled from "styled-components";
 
 export default function DropDownBox(props) {
   const { options, selected, onChange } = props;
@@ -12,8 +13,29 @@ export default function DropDownBox(props) {
         onChange={(e) => onChange(e)}
         value={selected}
         placeholder="Select an option"
+        className="dropdown-class"
+        menuClassName="menu-class"
+        placeholderClassName="placeholder-class"
+        controlClassName="control-class"
       />
     </DropdownWrapper>
   );
 }
-const DropdownWrapper = styled.div;
+const DropdownWrapper = styled.div`
+  padding: 10px;
+
+  .control-class {
+    background-color: blue;
+    color: white;
+    border-radius: 10px 10px 10px 10px;
+  }
+  .placeholder-class {
+  }
+  .dropdown-class {
+    background-color: red;
+    border-radius: 25px;
+  }
+  .menu-class {
+    border-radius: 0 0 10px 10px;
+  }
+`;
