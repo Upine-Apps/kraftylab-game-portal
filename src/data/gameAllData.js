680df1fab376a63257a9c93c9ae82090e5c6803a
Tate Walker <tatewalker@protonmail.com>
2022-11-30 03:55:00 -0600

    path update

--- src/data/gameAllData.js
+++ src/data/gameAllData.js
@@ -17,7 +17,7 @@ export const gameAllData = [
     color:
       "radial-gradient(218.51% 281.09% at 100% 100%, rgba(189, 202, 236, 0.6) 0%, rgba(233, 225, 255, 0.6) 45.83%, rgba(253, 63, 51, 0.6) 100%)",
     icon: "images/icons/imagePuzzleIcon.svg",
-    path: "https://games.kraftylab.com",
+    path: "https://games.kraftylab.com/puzzle",
     outsidePath: true,
   },
   {
--- src/service/SocketService.js
+++ src/service/SocketService.js
@@ -4,7 +4,7 @@ class SocketService {
 
   connect() {
     return new Promise((resolve, reject) => {
-      this.socket = io("games.kraftylab.com:6969");
+      this.socket = io("https://games.kraftylab.com:6969");
       if (!this.socket) {
         return reject();
       }
